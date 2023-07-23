6027444cf1887c1ef95357f2b22f2827a29cecd1
Phạm Kiên <taikhoancuaban203@gmail.com>
2023-07-24 00:18:52 +0700

    Add project files.

---
+++ 2.DATA/Models/Bills.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.DATA.Models
+{
+    public class Bills
+    {
+        public Guid ID { get; set; }
+        public string BillCode { get; set; }
+        public Guid CustomerID { get; set; }
+        public Guid StaffID { get; set; }
+        public DateTime DateCreate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Address { get; set; }
+        public bool Status { get; set; }
+    }
+}
